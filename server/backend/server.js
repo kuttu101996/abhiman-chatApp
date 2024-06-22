dae0feaaf28e59c4ef753d21df16da8f4d024995
tojoy123 <mail.joy@yahoo.com>
2024-06-22 15:18:17 +0530

    'deploy'

--- server/backend/server.js
+++ server/backend/server.js
@@ -42,7 +42,7 @@ const server = app.listen(process.env.port, async () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://abhiman-chat-app.netlify.app",
   },
 });
 
